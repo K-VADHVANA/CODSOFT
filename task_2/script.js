b4c4bcd83c4ba20ce9424d6bbc3eb0fe97ced531
khushi vadhvana <98330791+K-VADHVANA@users.noreply.github.com>
2024-06-23 19:16:19 +0530

    final commit

---
+++ task_2/script.js
@@ -0,0 +1,28 @@
+let input = document.getElementById('input');
+let btn =  document.querySelectorAll('button');
+
+let string = "";
+let array = Array.from(btn);
+array.forEach(button => {
+    button.addEventListener('click',(v) =>{
+        if(v.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(v.target.innerHTML == 'AC')
+            {
+                strinf = "";
+                input.value = string;
+            }
+            else if(v.target.innerHTML == 'DEl'){
+                string = string.substring(0, string.length-1);
+                input.value = string
+            }
+            else{
+                string += v.target.innerHTML;
+                input.value = string;
+            }
+        
+})
+
+})
